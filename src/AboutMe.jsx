71cd5a1eae101c22f7463bd287fab4a4756afe3c
Zak Dowsett <z.dowsett@outlook.com>
2020-03-09 21:28:40 +0000

    update links

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -47,16 +47,18 @@ const useStyles = makeStyles(theme => ({
         height: '15em',
         margin: '0 auto'
     },
+    icon: {
+        margin: '0.50em'
+    },
     links: {
         margin: '1em 20%',
         display: 'flex',
         flexDirection: 'row',
         flexGrow: 1,
-        justifyContent: 'space-evenly',
+        justifyContent: 'center',
         alignItems: 'center',
         '& a svg': {
-            background: 'white',
-            fontSize: '6em',
+            fontSize: '5em',
             '&:hover': {
                 boxShadow: '0 4px 20px 0 rgba(0,0,0,0.12)',
                 transform: 'scale(1.04)'
@@ -73,50 +75,49 @@ const useStyles = makeStyles(theme => ({
 
 export default function About(props) {
     const classes = useStyles();
+    const animation = useSpring({
+        to: {
+            height: 250,
+            top: -250
+        },   
+        from: {
+            height: 150,
+            top: -150
+        },
+        //reset: true,
+        reverse: true
+    });
 
-        const animation = useSpring({
-            to: {
-                height: 250,
-                top: -250
-            },   
-            from: {
-                height: 150,
-                top: -150
-            },
-            //reset: true,
-            reverse: true
-        });
-
-        const links = props.links.map((obj) => {
-            return (
-                <Link href={obj.link}>
-                    <SvgIcon htmlColor={obj.colour} className={classes.icon}>
-                        {ReactHtmlParser(obj.icon)}
-                    </SvgIcon>
-                </Link>
-            )
-        });
-
+    const links = props.links.map((obj, i) => {
         return (
-            <div id='about' style={{position: 'relative'}}>
-                <animated.div style={animation} className={classes.merge}></animated.div>
-                <div className={classes.root}>
-                    <div className={classes.avatarContainer}>
-                        <Avatar className={classes.avatar}>ZD</Avatar>
-                    </div>
-                    <div className={classes.secondContainer}>
-                        <Typography className={classes.text} variant="body1" color="primary">
-                            {props.children}
-                        </Typography>
-                        <div className={classes.links}>
-                            {links}
-                        </div>
+            <Link href={obj.link}>
+                <SvgIcon htmlColor={obj.colour} className={classes.icon}>
+                    {ReactHtmlParser(obj.icon)}
+                </SvgIcon>
+            </Link>
+        )
+    });
+
+    return (
+        <div id='about' style={{position: 'relative'}}>
+            <animated.div style={animation} className={classes.merge}></animated.div>
+            <div className={classes.root}>
+                <div className={classes.avatarContainer}>
+                    <Avatar className={classes.avatar}>ZD</Avatar>
+                </div>
+                <div className={classes.secondContainer}>
+                    <Typography className={classes.text} variant="body1" color="primary">
+                        {props.children}
+                    </Typography>
+                    <div className={classes.links}>
+                        {links}
                     </div>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className={classes.waves}>
-                    <path fill="rgb(34,195,145)" fill-opacity="1" d="M0,96L26.7,122.7C53.3,149,107,203,160,192C213.3,181,267,107,320,85.3C373.3,64,427,96,480,138.7C533.3,181,587,235,640,261.3C693.3,288,747,288,800,256C853.3,224,907,160,960,154.7C1013.3,149,1067,203,1120,213.3C1173.3,224,1227,192,1280,197.3C1333.3,203,1387,245,1413,266.7L1440,288L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"></path>]
-                </svg>
             </div>
-        );
-    }
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className={classes.waves}>
+                <path fill="rgb(34,195,145)" fill-opacity="1" d="M0,96L26.7,122.7C53.3,149,107,203,160,192C213.3,181,267,107,320,85.3C373.3,64,427,96,480,138.7C533.3,181,587,235,640,261.3C693.3,288,747,288,800,256C853.3,224,907,160,960,154.7C1013.3,149,1067,203,1120,213.3C1173.3,224,1227,192,1280,197.3C1333.3,203,1387,245,1413,266.7L1440,288L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"></path>]
+            </svg>
+        </div>
+    );
+}
 
