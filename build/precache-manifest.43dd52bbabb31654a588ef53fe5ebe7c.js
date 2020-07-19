f7423e0a1314c022cbe35b750a2bf86082a65c01
Zak Dowsett <z.dowsett@outlook.com>
2020-07-19 20:15:42 +0100

    initial deployment

---
+++ build/precache-manifest.43dd52bbabb31654a588ef53fe5ebe7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ed354ffb184c3d562291a31130226c",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "b414ecc5bfff5399587e",
+    "url": "/my-portfolio/static/css/main.25ec1e4f.chunk.css"
+  },
+  {
+    "revision": "3462a04cebe0d7fa0fff",
+    "url": "/my-portfolio/static/js/2.de659a57.chunk.js"
+  },
+  {
+    "revision": "17948ada92d714c0fcae27f4faf31183",
+    "url": "/my-portfolio/static/js/2.de659a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b414ecc5bfff5399587e",
+    "url": "/my-portfolio/static/js/main.da1f0efb.chunk.js"
+  },
+  {
+    "revision": "164cd71dc8e56a933ce3",
+    "url": "/my-portfolio/static/js/runtime-main.d96a2d46.js"
+  }
+]);
