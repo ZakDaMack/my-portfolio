07a681c26d47d521e228c1a8a41c8b0447241f52
Zak Dowsett <z.dowsett@outlook.com>
2020-08-12 01:15:58 +0100

    fix baseline

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.54c8a349616d6b2bcd3f9357aa4ba380.js"
+  "./precache-manifest.21039e5c3a3dc002b6957425329d9d7f.js"
 );
 
 self.addEventListener('message', (event) => {
